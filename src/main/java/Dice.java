21e2cc8d5a64af5ca8ded6cd861cc6f77df9fd97
eriinys <eriinys@duck.com>
2025-10-02 10:47:05 -0500

    initial commit

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Dice {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        int roll1;
+        int roll2;
+    }
+
+    public int roll(){
+        int maxValue = 6;
+        int minValue = 1;
+        int randomNumber = (int)(Math.random() * maxValue) + minValue;
+        System.out.println(randomNumber);
+        return randomNumber;
+        }
 }
